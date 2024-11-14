2393f3850b7a1e2c320c2ea3fbbf6b85fba9af25
Jenish Ghadiya <jenishvghadiya@gmail.com>
2024-11-14 12:54:00 +0530

    initial commit

--- src/componets/auth/forgtpassword/ForgotPasswordOTP.jsx
+++ src/componets/auth/forgtpassword/ForgotPasswordOTP.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams , useSearchParams} from 'react-router-dom';
 import toast from 'react-hot-toast';
 import '../auth.scss';
 
